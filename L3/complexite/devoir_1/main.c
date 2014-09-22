58e0942cad1b1160de5459ca8e97e1f81cc46378
Jonathan BAUDIN <jjbaudin@gmail.com>
2014-09-22 16:28:12 +0200

    Ajout fichier base et Makefile

---
+++ L3/complexite/devoir_1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main( int argc, char** argv )
+{
+    (void) argc;
+    (void) argv;
+    printf( "TODO:\
+            \n\t1. Tri par insertion séquentiel\
+            \n\t2. Tri par insertion dichotomique\
+            \n\t3. Tri par selection-permutation\
+            \n\t4. Tri à bulle\
+            \n\t5. Tri par fusion\
+            \n\t6. Tri rapide (QuickSort)\
+            \n\t7. Tri à l'aide d'arbre binaire de recherche\
+            \n\t8. Tri par tas\
+            \n\n\nAu BOULOT !!!!!\n\n" );
+
+    return 0;
+}
